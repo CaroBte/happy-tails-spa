a40b5e9ffce27c47b7412eaa44baf1f68c914d10
CaroBte <karito0721@hotmail.com>
2023-02-27 23:31:31 -0500

    feat: project init

---
+++ src/api/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyADoxXDSOeEFGG3faespn_p-VriUDIcN98",
+    authDomain: "happy-tails-spa.firebaseapp.com",
+    projectId: "happy-tails-spa",
+    storageBucket: "happy-tails-spa.appspot.com",
+    messagingSenderId: "528464777939",
+    appId: "1:528464777939:web:cca8114acd5eba6e8a7a26"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
