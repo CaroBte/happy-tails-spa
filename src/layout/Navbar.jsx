e75ab07b3a32e4bbd9f88083de9e17eb41e390e6
Bney28 <bney1@outlook.com>
2023-03-01 22:19:39 -0500

    fix: styles of navbar web and mobile

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,58 +5,59 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <div className="menu-web d-flex flex-column">
+            <div className='menu-web'>
+                <div className="d-flex flex-column fixed-left">
 
-                <div className="name_logo d-flex flex-column align-items-center">
-                    <i className="fa-solid fa-paw h3"></i>
-                    <h1>Happy Tails</h1>
-                    <h1>Spa</h1>
-                </div>
-
-                <div className="navbar-nav div_ppal d-flex flex-column">
-
-                    <div className="list_one d-flex flex-column">
-
-                        <Link to={"/buscar"} className="nav-item mx-3">
-                            <i className="fa-solid fa-magnifying-glass h3"></i>
-                            Buscar
-                        </Link>
-
-                        <Link to={"/"} className="nav-item mx-3">
-                            <i className="fa-solid fa-house-chimney-user h3"></i>
-                            Home
-                        </Link>
-
-                        <Link to={"/carrito"} className="nav-item mx-3">
-                            <i className="fa-solid fa-cart-shopping h3"></i>
-                            Carrito
-                        </Link>
-
-                        <Link to={"/nosotros"} className="nav-item mx-3">
-                            <i className="fa-solid fa-paw h3"></i>
-                            Nosotros
-                        </Link>
-
-                        <Link to={"/perfil"} className="nav-item mx-3">
-                            <i className="fa-solid fa-user h3"></i>
-                            Perfil
-                        </Link>
+                    <div className="name_logo d-flex flex-column align-items-center">
+                        <i className="fa-solid fa-paw h3"></i>
+                        <h1>Happy Tails</h1>
+                        <h1>Spa</h1>
                     </div>
 
-                    <div className="list_two d-flex flex-column">
-                        <Link className="nav-item mx-3">
-                            <i className="fa-solid fa-circle-half-stroke h3"></i>
-                            Tema
-                        </Link>
+                    <div className="navbar-nav div_ppal d-flex flex-column">
 
-                        <Link className="nav-item mx-3">
-                            <i className="fa-solid fa-mobile-screen h3"></i>
-                            Contáctanos
-                        </Link>
+                        <div className="list_one d-flex flex-column">
+
+                            <Link to={"/buscar"} className="nav-item mx-3">
+                                <i className="fa-solid fa-magnifying-glass h3"></i>
+                                Buscar
+                            </Link>
+
+                            <Link to={"/"} className="nav-item mx-3">
+                                <i className="fa-solid fa-house-chimney-user h3"></i>
+                                Home
+                            </Link>
+
+                            <Link to={"/carrito"} className="nav-item mx-3">
+                                <i className="fa-solid fa-cart-shopping h3"></i>
+                                Carrito
+                            </Link>
+
+                            <Link to={"/nosotros"} className="nav-item mx-3">
+                                <i className="fa-solid fa-paw h3"></i>
+                                Nosotros
+                            </Link>
+
+                            <Link to={"/perfil"} className="nav-item mx-3">
+                                <i className="fa-solid fa-user h3"></i>
+                                Perfil
+                            </Link>
+                        </div>
+
+                        <div className="list_two d-flex flex-column">
+                            <Link className="nav-item mx-3">
+                                <i className="fa-solid fa-circle-half-stroke h3"></i>
+                                Tema
+                            </Link>
+
+                            <Link to={"/nosotros"} className="nav-item mx-3">
+                                <i className="fa-solid fa-mobile-screen h3"></i>
+                                Contáctanos
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
-
 
 
             <div className="menu-burguer">
@@ -128,7 +129,7 @@ const Navbar = () => {
                                             Tema
                                         </Link>
 
-                                        <Link className="nav-item mx-3">
+                                        <Link to={"/nosotros"} className="nav-item mx-3">
                                             <i className="fa-solid fa-mobile-screen h3"></i>
                                             Contáctanos
                                         </Link>
