285a09c399661751db106717cda6cc2e48231a4e
Bney28 <bney1@outlook.com>
2023-03-03 10:02:42 -0500

    fix: navbar styles

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,7 +5,7 @@ import { MenuWeb, MenuBurger } from '../components'
 const Navbar = () => {
     return (
         <>
-            <div className='menu-web'>
+            <div className='menu-web border-end border-danger-subtle'>
                 <MenuWeb />
             </div>
 
