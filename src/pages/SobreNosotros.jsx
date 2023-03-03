444f90f99eb8135d86eb640c04df5aff124c5582
Bney28 <bney1@outlook.com>
2023-03-03 11:56:24 -0500

    feat: Sobre Nosotros Page construction

--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -2,7 +2,60 @@ import React from 'react'
 
 const SobreNosotros = () => {
     return (
-        <div>SobreNosotros</div>
+        <>
+            <div className="screen row gx-0 justify-content-end">
+                <div className="cont_nosotros col-md-9">
+                    <div className='d-flex align-items-baseline mx-4 mt-5'>
+                        <i className="user-icon h3 fa-solid fa-paw"></i>
+                        <h5 className='mx-2'>Sobre Nosotros</h5>
+                    </div>
+
+                    <div className="textos">
+                        <h6>
+                            HAPPY TAILS SPA nace desde la necesidad de poder darle un cuidado especial
+                            a nuestras mascotas sin que trasladarlas sea un inconveniente tanto para ellos
+                            como para los mamás y papás gatunos y perrunos.
+                        </h6>
+
+                        <h5>
+                            Nuestros Clientes
+                        </h5>
+                        
+                        <h6>
+                            Foticos de mascotas felices
+                            <img src="" alt="pet-image" />
+                        </h6>
+
+                        <h5>
+                            Contáctanos
+                        </h5>
+
+                        <h6>
+                            <ul>
+                                <li>Estamos ubicados en</li>
+                                <li>No dudes en llamarnos o escribirnos por nuestro Whatsapp</li>
+                                <li>O también puedes escribirnos a nuestro correo</li>
+                            </ul>
+                        </h6>
+
+                        <h5>
+                            Nuestros Valores
+                        </h5>
+
+                        <h6>
+                            <ul>
+                                <li>Amor por las mascotas.</li>
+                                <li>Responsabilidad</li>
+                                <li>Respeto</li>
+                                <li>Solidaridad</li>
+                                <li>Sentido de Pertenencia</li>
+                            </ul>
+                        </h6>
+                    </div>
+
+                </div>
+            </div>
+        </>
     )
 }
 
