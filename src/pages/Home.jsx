fc47e765dcaeba7ec55ab95058357e15a8cbc971
Bney28 <bney1@outlook.com>
2023-03-03 22:03:50 -0500

    fix: Banner Home styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,11 +19,11 @@ const Home = () => {
           <div className="carrusel">
             <img className='w-50' src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677594212/Happy%20Tails%20Spa%20App/image-33462-800_f511om.jpg" alt="baño-perro" />
             <img className='w-50' src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677767477/Happy%20Tails%20Spa%20App/cat_20getting_20bathed_20in_20a_20bath_20tub-DP.jpg_ccfhzx.jpg" alt="baño-gato" />
-            <img className='w-50' src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677540263/Happy%20Tails%20Spa%20App/a-cat-sleeping-on-a-massage-table-while-taking-spa-treatments_yvr61l.jpg" alt="spa-gato" />
-            <img className='w-50' src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677540264/Happy%20Tails%20Spa%20App/56-560788_4paws-mobile-dog-spa-and-mobile-cat-spa_r9ccfm.jpg" alt="baño-perro" />
-            {/*<img src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677594212/Happy%20Tails%20Spa%20App/how-often-should-you-wash-dog_qlsheo.jpg" alt="baño-perro" />
-            <img src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677767477/Happy%20Tails%20Spa%20App/bath-kitty_pekqt6.png" alt="baño-gato" />
-            <img src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677594211/Happy%20Tails%20Spa%20App/dog-wash_a9h7hc.jpg" alt="baño-perro" /> */}
+            {/* <img className='w-50' src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677540263/Happy%20Tails%20Spa%20App/a-cat-sleeping-on-a-massage-table-while-taking-spa-treatments_yvr61l.jpg" alt="spa-gato" />
+            <img className='w-50' src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677540264/Happy%20Tails%20Spa%20App/56-560788_4paws-mobile-dog-spa-and-mobile-cat-spa_r9ccfm.jpg" alt="baño-perro" /> */}
+          </div>
+          <div className="bienvenidos text-uppercase">
+            <h1 className='fw-bold'>¡Bienvenidos!</h1>
           </div>
           <h4 className='m-4 pb-3 border-bottom'><i className="fa-solid fa-bath user-icon"></i>  Nuestros Servicios</h4>
           <div className='container-cards d-flex flex-wrap justify-content-around'>
