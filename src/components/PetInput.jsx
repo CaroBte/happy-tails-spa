0d0cfcc03aecec255bfcc920fc8fd53c904150ca
Jaredzu <jared.zu96@gmail.com>
2023-03-09 18:10:03 -0500

    feat: add Formik basic estructure

--- src/components/PetInput.jsx
+++ src/components/PetInput.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Formik, Field, Form } from 'formik'
 
 
 const PetInput = () => {
+
+    const handleSubmit = (values) => {
+        console.log(values, "Values de la mascota");
+    }
+
     return (
         <>
             <div className='text-center my-4'>
@@ -10,24 +16,50 @@ const PetInput = () => {
                 </button>
             </div>
             <div className="collapse w-100 my-2" id="collapseExample">
-                <form className="form-login mx-4 ">
-                    <label className='d-flex flex-column form-label title '>Nombre de tu Mascota
-                        <input className='border-0 border-bottom input-login' type="text" />
-                    </label>
-                    <label className='d-flex flex-column form-label title '>Especie
-                        <select className='border-0 border-bottom input-login'>
-                            <option value="Perro">Perro</option>
-                            <option value="Gato">Gato</option>
-                        </select>
-                    </label>
-                    <label className='d-flex flex-column form-label title'>Imagen
-                        <input className='border-0 border-bottom input-login' type="text" />
-                    </label>
-                    <label className='d-flex flex-column form-label title '>Edad
-                        <input className='border-0 border-bottom input-login' type="text" />
-                    </label>
-                </form>
-                <button className='btn btn-edit mx-4 mb-4' type='submit'>Registrar</button>
+
+                <Formik
+                    initialValues={{
+                        petNombre: "",
+                        especie: "",
+                        imagen: "",
+                        edad: ""
+                    }}
+
+                    onSubmit={handleSubmit}
+                >
+                    {() => (
+
+                        <Form className="form-login mx-4 ">
+
+                            <label className='d-flex flex-column form-label title '>Nombre de tu Mascota
+                                <Field className='border-0 border-bottom input-login' name="petNombre" type="text" />
+                            </label>
+
+                            <label className='d-flex flex-column form-label title '>Especie
+                                <Field name="especie" as="select" className='border-0 border-bottom input-login'>
+
+                                    <option disabled value="">Selecciona</option>
+                                    <option value="Perro">Perro</option>
+                                    <option value="Gato">Gato</option>
+                                </Field>
+                            </label>
+
+                            <label className='d-flex flex-column form-label title'>Imagen
+                                <Field className='border-0 border-bottom input-login' name="imagen" type="text" />
+                            </label>
+
+                            <label className='d-flex flex-column form-label title '>Edad
+                                <Field className='border-0 border-bottom input-login' name="edad" type="text" />
+                            </label>
+
+                            <button className='btn btn-edit mx-4 mb-4' type='submit'>Registrar</button>
+                        </Form>
+                    )}
+
+
+                </Formik>
+
+
             </div>
         </>
     )
--- src/components/Usuario.jsx
+++ src/components/Usuario.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import PetCard from './PetCard'
 import PetInput from './PetInput'
+import { Formik, Field, Form } from 'formik'
 
 const Usuario = () => {
+
+    const handleSubmit = (values) => {
+        console.log(values);
+    }
+
     return (
         <>
             <div className="screen row gx-0">
@@ -17,32 +23,62 @@ const Usuario = () => {
                         <img className='m-auto rounded-circle image-profile ' src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1677613038/Happy%20Tails%20Spa%20App/Account-Avatar-Profile-PNG-Photo_nqxxzd.png" width={"20%"} alt="User Logo" />
                     </div>
 
-                    <form className="form-login mx-4 mb-5 ">
-                        <label className='d-flex flex-column form-label title '>Nombre
-                            <input className='border-0 border-bottom input-login' type="text" />
-                            <i className="fa-solid fa-pencil opacity-25"></i>
-                        </label>
-                        <label className='d-flex flex-column form-label title '>Correo Electrónico
-                            <input className='border-0 border-bottom input-login' type="text" />
-                            <i className="fa-solid fa-pencil opacity-25"></i>
-                        </label>
-                        <label className='d-flex flex-column form-label title '>Contraseña
-                            <input className='border-0 border-bottom input-login' type="password" autoComplete="current-password"/>
-                            <i className="fa-solid fa-pencil opacity-25"></i>
-                        </label>
-                        <label className='d-flex flex-column form-label title '>Teléfono
-                            <input className='border-0 border-bottom input-login' type="text" />
-                            <i className="fa-solid fa-pencil opacity-25"></i>
-                        </label>
-                        <label className='d-flex flex-column form-label title '>Dirección de Domicilio
-                            <input className='border-0 border-bottom input-login' type="text" />
-                            <i className="fa-solid fa-pencil opacity-25"></i>
-                        </label>
-                    </form>
-                    <div className="btns-profile-web">
-                        <button className="btn btn-login mx-2 mb-2 mx-md-1 ">Guardar</button>
-                        <button className="btn btn-logout mx-2 mb-2 mx-md-1 ">Log Out</button>
-                    </div>
+                    <Formik
+
+                        initialValues={{
+                            nombre: "",
+                            correo: "",
+                            contrasena: "",
+                            telefono: "",
+                            direccion: "",
+                            imagen: ""
+                        }}
+
+                        onSubmit={handleSubmit}
+
+                    >
+
+                        {() => (
+
+                            <Form className="form-login mx-4 mb-5 ">
+
+                                <label className='d-flex flex-column form-label title '>Nombre
+                                    <Field className='border-0 border-bottom input-login' name='nombre' type="text" />
+                                    <i className="fa-solid fa-pencil opacity-25"></i>
+                                </label>
+
+                                <label className='d-flex flex-column form-label title '>Correo Electrónico
+                                    <Field className='border-0 border-bottom input-login' name='correo' type="email" />
+                                    <i className="fa-solid fa-pencil opacity-25"></i>
+                                </label>
+
+                                <label className='d-flex flex-column form-label title '>Contraseña
+                                    <Field className='border-0 border-bottom input-login' name='contrasena' type="password" autoComplete="current-password" />
+                                    <i className="fa-solid fa-pencil opacity-25"></i>
+                                </label>
+
+                                <label className='d-flex flex-column form-label title '>Teléfono
+                                    <Field className='border-0 border-bottom input-login' name='telefono' type="tel" />
+                                    <i className="fa-solid fa-pencil opacity-25"></i>
+                                </label>
+
+                                <label className='d-flex flex-column form-label title '>Dirección de Domicilio
+                                    <Field className='border-0 border-bottom input-login' name='direccion' type="text" />
+                                    <i className="fa-solid fa-pencil opacity-25"></i>
+                                </label>
+
+                                {/* <Field className="btn btn-primary" name="imagen" type="file" /> */}
+
+                                <div className="btns-profile-web">
+                                    <button className="btn btn-login mx-2 mb-2 mx-md-1 " type='submit'>Guardar</button>
+                                    <button className="btn btn-logout mx-2 mb-2 mx-md-1 ">Log Out</button>
+                                </div>
+
+                            </Form>
+
+                        )}
+
+                    </Formik>
                 </div>
 
                 <div className="my-pets px-4 mb-5 col-md-4 d-flex flex-column justify-content-center align-items-center">
