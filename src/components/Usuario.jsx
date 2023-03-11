5fced07b95e514055b11bedc741d25d146feb232
Jaredzu <jared.zu96@gmail.com>
2023-03-11 12:18:20 -0500

    feat: add validations in "usuario" form to firebase

--- src/components/Usuario.jsx
+++ src/components/Usuario.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
 import PetCard from './PetCard'
 import PetInput from './PetInput'
-import { Formik, Field, Form } from 'formik'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
 import { authAPI } from '../api/'
 import { usuario } from '../context'
+import Swal from 'sweetalert2'
 
 const Usuario = () => {
 
@@ -12,6 +13,14 @@ const Usuario = () => {
     const handleSubmit = async (values) => {
         await enviarUsuario(values, usuarioActual.id)
         await traerUsuario(usuarioActual.id)
+
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Datos actualizados',
+            showConfirmButton: false,
+            timer: 2000
+        })
     }
 
     const handleLogout = () => {
@@ -49,35 +58,116 @@ const Usuario = () => {
                             imagen: `${usuarioActual.imagen}`
                         }}
 
+                        /* VALIDATIONS FORM */
+
+                        validate={(values) => {
+                            let errores = {}
+
+                            // Validate Nombre
+                            if (!values.nombre) {
+
+                                errores.nombre = "Por favor ingresa tu nombre"
+
+                            } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.nombre)) {
+                                errores.nombre = "El nombre solo puede contener letras y espacios"
+                            }
+
+                            // Validate Correo
+                            if (!values.correo) {
+
+                                errores.correo = "Por favor ingresa tu correo"
+
+                            } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.correo)) {
+                                errores.correo = "El correo debe ser válido"
+                            }
+
+                            // Validate Imagen
+                            if (!values.imagen) {
+
+                                errores.imagen = "Por favor ingresa una URL de tu imagen"
+
+                            } else if (!/^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/.test(values.imagen)) {
+                                errores.imagen = "Ingresa una URL válida"
+                            }
+
+                            // Validate Teléfono
+                            if (!values.telefono) {
+
+                                errores.telefono = "Por favor ingresa tu número de contacto"
+
+                            } else if (values.telefono.length < 7 || values.telefono.length > 10) {
+                                errores.telefono = "Ingresa un teléfonos entre 7 y 10 dígitos"
+
+                            } else if (!/^[0-9]+$/.test(values.telefono)) {
+
+                                errores.telefono = "Por favor solo ingresa números sin espacios"
+
+                            }
+
+                            // Validate Dirección
+                            if (!values.direccion) {
+
+                                errores.direccion = "Por favor ingresa tu ciudad y dirección de domicilio"
+                            }
+
+                            return errores
+
+                        }}
+
                         onSubmit={handleSubmit}>
 
-                        {() => (
-
+                        {({ errors }) => (
                             <Form className="form-login mx-4 mb-5 ">
 
                                 <label className='d-flex flex-column form-label title '>Nombre
                                     <Field className='border-0 border-bottom input-login' name='nombre' type="text" />
                                     <i className="fa-solid fa-pencil opacity-25"></i>
+                                    <ErrorMessage name='nombre' component={() => (
+                                        <div>
+                                            {errors.nombre}
+                                        </div>
+                                    )} />
+
                                 </label>
 
                                 <label className='d-flex flex-column form-label title '>Correo Electrónico
                                     <Field className='border-0 border-bottom input-login' name='correo' type="email" />
                                     <i className="fa-solid fa-pencil opacity-25"></i>
+                                    <ErrorMessage name='correo' component={() => (
+                                        <div>
+                                            {errors.correo}
+                                        </div>
+                                    )} />
                                 </label>
 
                                 <label className='d-flex flex-column form-label title '>URL imagen de perfil
                                     <Field className='border-0 border-bottom input-login' name='imagen' type="text" />
                                     <i className="fa-solid fa-pencil opacity-25"></i>
+                                    <ErrorMessage name='imagen' component={() => (
+                                        <div>
+                                            {errors.imagen}
+                                        </div>
+                                    )} />
                                 </label>
 
                                 <label className='d-flex flex-column form-label title '>Teléfono
                                     <Field className='border-0 border-bottom input-login' name='telefono' type="tel" />
                                     <i className="fa-solid fa-pencil opacity-25"></i>
+                                    <ErrorMessage name='telefono' component={() => (
+                                        <div>
+                                            {errors.telefono}
+                                        </div>
+                                    )} />
                                 </label>
 
                                 <label className='d-flex flex-column form-label title '>Dirección de Domicilio
                                     <Field className='border-0 border-bottom input-login' name='direccion' type="text" />
                                     <i className="fa-solid fa-pencil opacity-25"></i>
+                                    <ErrorMessage name='direccion' component={() => (
+                                        <div>
+                                            {errors.direccion}
+                                        </div>
+                                    )} />
                                 </label>
 
                                 {/* <Field className="btn btn-primary" name="imagen" type="file" /> */}
