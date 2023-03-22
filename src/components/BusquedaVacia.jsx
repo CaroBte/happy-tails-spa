f20099c6ddfc68836356212cdff4eaef5962f790
Bney28 <bney1@outlook.com>
2023-03-21 19:26:26 -0500

    fix: size of "busquedaVacia"

--- src/components/BusquedaVacia.jsx
+++ src/components/BusquedaVacia.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const BusquedaVacia = () => {
     return (
         <div className='mb-5 d-flex flex-column align-items-center'>
-            <h1 className='mt-5 text-center'>Ups! No encontramos lo que buscas</h1>
-            <h1 className='mb-5'>Inténtalo de nuevo</h1>
+            <h1 className='mt-5 h3 text-center'>Ups! No encontramos lo que buscas</h1>
+            <h1 className='mb-5 h5 text-center'>Inténtalo de nuevo</h1>
             <img className='rounded-5 w-100' src="https://res.cloudinary.com/dwm5krz7f/image/upload/v1677795923/Happy%20Tails%20Spa/28597f58dce60678af724923654b14d7_nss3cw.png" alt="pets" />
         </div>
     )
